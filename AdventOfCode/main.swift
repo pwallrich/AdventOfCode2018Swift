3fcd2b2da85a2d5bbac8a82aa2d53ff2961e802c
Philipp Wallrich <pwallrich@inovex.de>
2018-12-15 22:05:43 +0100

    Initial Commit

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Philipp Wallrich on 15.12.18.
+//  Copyright © 2018 RATIONAL. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
